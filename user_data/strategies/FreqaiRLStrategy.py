b2278165805cff74c65ffe9c2f39fe61768c4dc9
Avi Pilcer <avi@thefittingroom.tech>
2024-02-02 12:25:47 -0300

    -

--- user_data/strategies/FreqaiRLStrategy.py
+++ user_data/strategies/FreqaiRLStrategy.py
@@ -12,7 +12,7 @@ from freqtrade.strategy import IStrategy
 logger = logging.getLogger(__name__)
 
 
-class FreqaiExampleStrategy(IStrategy):
+class FreqaiRLStrategy(IStrategy):
     """
     Example strategy showing how the user connects their own
     IFreqaiModel to the strategy.
@@ -45,99 +45,6 @@ class FreqaiExampleStrategy(IStrategy):
     startup_candle_count: int = 40
     can_short = True
 
-    def feature_engineering_expand_all(self, dataframe: DataFrame, period: int,
-                                       metadata: Dict, **kwargs) -> DataFrame:
-        """
-        *Only functional with FreqAI enabled strategies*
-        This function will automatically expand the defined features on the config defined
-        `indicator_periods_candles`, `include_timeframes`, `include_shifted_candles`, and
-        `include_corr_pairs`. In other words, a single feature defined in this function
-        will automatically expand to a total of
-        `indicator_periods_candles` * `include_timeframes` * `include_shifted_candles` *
-        `include_corr_pairs` numbers of features added to the model.
-
-        All features must be prepended with `%` to be recognized by FreqAI internals.
-
-        Access metadata such as the current pair/timeframe with:
-
-        `metadata["pair"]` `metadata["tf"]`
-
-        More details on how these config defined parameters accelerate feature engineering
-        in the documentation at:
-
-        https://www.freqtrade.io/en/latest/freqai-parameter-table/#feature-parameters
-
-        https://www.freqtrade.io/en/latest/freqai-feature-engineering/#defining-the-features
-
-        :param dataframe: strategy dataframe which will receive the features
-        :param period: period of the indicator - usage example:
-        :param metadata: metadata of current pair
-        dataframe["%-ema-period"] = ta.EMA(dataframe, timeperiod=period)
-        """
-
-        dataframe["%-rsi-period"] = ta.RSI(dataframe, timeperiod=period)
-        dataframe["%-mfi-period"] = ta.MFI(dataframe, timeperiod=period)
-        dataframe["%-adx-period"] = ta.ADX(dataframe, timeperiod=period)
-        dataframe["%-sma-period"] = ta.SMA(dataframe, timeperiod=period)
-        dataframe["%-ema-period"] = ta.EMA(dataframe, timeperiod=period)
-
-        bollinger = qtpylib.bollinger_bands(
-            qtpylib.typical_price(dataframe), window=period, stds=2.2
-        )
-        dataframe["bb_lowerband-period"] = bollinger["lower"]
-        dataframe["bb_middleband-period"] = bollinger["mid"]
-        dataframe["bb_upperband-period"] = bollinger["upper"]
-
-        dataframe["%-bb_width-period"] = (
-            dataframe["bb_upperband-period"]
-            - dataframe["bb_lowerband-period"]
-        ) / dataframe["bb_middleband-period"]
-        dataframe["%-close-bb_lower-period"] = (
-            dataframe["close"] / dataframe["bb_lowerband-period"]
-        )
-
-        dataframe["%-roc-period"] = ta.ROC(dataframe, timeperiod=period)
-
-        dataframe["%-relative_volume-period"] = (
-            dataframe["volume"] / dataframe["volume"].rolling(period).mean()
-        )
-
-        return dataframe
-
-    def feature_engineering_expand_basic(
-            self, dataframe: DataFrame, metadata: Dict, **kwargs) -> DataFrame:
-        """
-        *Only functional with FreqAI enabled strategies*
-        This function will automatically expand the defined features on the config defined
-        `include_timeframes`, `include_shifted_candles`, and `include_corr_pairs`.
-        In other words, a single feature defined in this function
-        will automatically expand to a total of
-        `include_timeframes` * `include_shifted_candles` * `include_corr_pairs`
-        numbers of features added to the model.
-
-        Features defined here will *not* be automatically duplicated on user defined
-        `indicator_periods_candles`
-
-        All features must be prepended with `%` to be recognized by FreqAI internals.
-
-        Access metadata such as the current pair/timeframe with:
-
-        `metadata["pair"]` `metadata["tf"]`
-
-        More details on how these config defined parameters accelerate feature engineering
-        in the documentation at:
-
-        https://www.freqtrade.io/en/latest/freqai-parameter-table/#feature-parameters
-
-        https://www.freqtrade.io/en/latest/freqai-feature-engineering/#defining-the-features
-
-        :param dataframe: strategy dataframe which will receive the features
-        :param metadata: metadata of current pair
-        dataframe["%-pct-change"] = dataframe["close"].pct_change()
-        dataframe["%-ema-200"] = ta.EMA(dataframe, timeperiod=200)
-        """
-
-        return dataframe
 
     def feature_engineering_standard(self, dataframe: DataFrame, **kwargs) -> DataFrame:
         # The following features are necessary for RL models
@@ -166,35 +73,22 @@ class FreqaiExampleStrategy(IStrategy):
         dataframe["&-action"] = 0
         return dataframe
 
+
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-
-        # All indicators must be populated by feature_engineering_*() functions
-
-        # the model will return all labels created by user in `set_freqai_targets()`
-        # (& appended targets), an indication of whether or not the prediction should be accepted,
-        # the target mean/std values for each of the labels created by user in
-        # `set_freqai_targets()` for each training period.
-
         dataframe = self.freqai.start(dataframe, metadata, self)
-
         return dataframe
+
 
     def populate_entry_trend(self, df: DataFrame, metadata: dict) -> DataFrame:
 
-        enter_long_conditions = [
-            df["do_predict"] == 1,
-            df["&-s_close"] > 0.01,
-            ]
+        enter_long_conditions = [df["do_predict"] == 1, df["&-action"] == 1]
 
         if enter_long_conditions:
             df.loc[
                 reduce(lambda x, y: x & y, enter_long_conditions), ["enter_long", "enter_tag"]
             ] = (1, "long")
 
-        enter_short_conditions = [
-            df["do_predict"] == 1,
-            df["&-s_close"] < -0.01,
-            ]
+        enter_short_conditions = [df["do_predict"] == 1, df["&-action"] == 3]
 
         if enter_short_conditions:
             df.loc[
@@ -204,43 +98,37 @@ class FreqaiExampleStrategy(IStrategy):
         return df
 
     def populate_exit_trend(self, df: DataFrame, metadata: dict) -> DataFrame:
-        exit_long_conditions = [
-            df["do_predict"] == 1,
-            df["&-s_close"] < 0
-            ]
+        exit_long_conditions = [df["do_predict"] == 1, df["&-action"] == 2]
         if exit_long_conditions:
             df.loc[reduce(lambda x, y: x & y, exit_long_conditions), "exit_long"] = 1
 
-        exit_short_conditions = [
-            df["do_predict"] == 1,
-            df["&-s_close"] > 0
-            ]
+        exit_short_conditions = [df["do_predict"] == 1, df["&-action"] == 4]
         if exit_short_conditions:
             df.loc[reduce(lambda x, y: x & y, exit_short_conditions), "exit_short"] = 1
 
         return df
 
-    def confirm_trade_entry(
-        self,
-        pair: str,
-        order_type: str,
-        amount: float,
-        rate: float,
-        time_in_force: str,
-        current_time,
-        entry_tag,
-        side: str,
-        **kwargs,
-    ) -> bool:
+    # def confirm_trade_entry(
+    #     self,
+    #     pair: str,
+    #     order_type: str,
+    #     amount: float,
+    #     rate: float,
+    #     time_in_force: str,
+    #     current_time,
+    #     entry_tag,
+    #     side: str,
+    #     **kwargs,
+    # ) -> bool:
 
-        df, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
-        last_candle = df.iloc[-1].squeeze()
+    #     df, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
+    #     last_candle = df.iloc[-1].squeeze()
 
-        if side == "long":
-            if rate > (last_candle["close"] * (1 + 0.0025)):
-                return False
-        else:
-            if rate < (last_candle["close"] * (1 - 0.0025)):
-                return False
+    #     if side == "long":
+    #         if rate > (last_candle["close"] * (1 + 0.0025)):
+    #             return False
+    #     else:
+    #         if rate < (last_candle["close"] * (1 - 0.0025)):
+    #             return False
 
-        return True
+    #     return True
